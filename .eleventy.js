bb06f71d81fa21efd463f9229c879c6e723cc197
Juan Bernardo Benavides Rubio <jbbenavidesr@unal.edu.co>
2021-06-09 22:19:09 -0500

    Set up css settings

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,31 @@
 // Filters
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
+const CleanCSS = require("clean-css");
 
 module.exports = (config) => {
-  // Returns a collection with all entries of the gospel in reverse date oder
-  config.addCollection("evangelios", (collection) => {
-    return [...collection.getFilteredByGlob("./src/evangelios/*.md")].reverse();
-  });
+    // Returns a collection with all entries of the gospel in reverse date oder
+    config.addCollection("evangelios", (collection) => {
+        return [
+            ...collection.getFilteredByGlob("./src/evangelios/*.md"),
+        ].reverse();
+    });
 
-  // Add filters
-  config.addFilter("dateFilter", dateFilter);
-  config.addFilter("w3DateFilter", w3DateFilter);
+    // Add filters
+    config.addFilter("dateFilter", dateFilter);
+    config.addFilter("w3DateFilter", w3DateFilter);
+    config.addFilter("cssmin", function (code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
 
-  return {
-    markdownTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
+    return {
+        markdownTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
 
-    dir: {
-      input: "src",
-      output: "dist",
-    },
-  };
+        dir: {
+            input: "src",
+            output: "dist",
+        },
+    };
 };
